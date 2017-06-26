0afc9363de8e348b52a8da90582c40e647bcabf7
adir-ch <actiger@gmail.com>
2017-06-26 21:28:23 +1000

    Photo store initial commit

---
+++ PhotoStudio/Website/Controllers/StoreController.cs
@@ -0,0 +1,25 @@
+﻿using PhotoStudio.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Website.ViewModels;
+
+namespace Website.Controllers
+{
+    [RoutePrefix("Store")]
+    public class StoreController : Controller
+    {
+        private PhotoStudioContext db = new PhotoStudioContext();
+
+        // GET: Store
+        [Route("")]
+        public ActionResult Index()
+        {
+            var vm = new StoreIndexViewModel();
+            vm.Photos = db.GetAvailablePhotos(); 
+            return View();
+        }
+    }
+}
---
+++ PhotoStudio/Website/ViewModels/StoreIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PhotoStudio.Entities; 
+
+namespace Website.ViewModels
+{
+    public class StoreIndexViewModel
+    {
+        public List<Photo> Photos { get; set; }
+    }
+}
